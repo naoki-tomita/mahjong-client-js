0a1188929d9df84172523e970ba8ddcc0c082d2b
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2016-11-09 23:17:52 +0900

    add color-collector

--- color-collector.js
+++ color-collector.js
@@ -1,7 +1,8 @@
 var MahjongMan = {
-  initialize: function( options ) {
+  init: function( options ) {
     this.id = options.game.id;
-    this.Pais = options.kyoku.tehais[ this.id ];
+    this.pais = options.kyoku.tehais[ this.id ];
+    this.color = ( [ "m", "p", "s" ] )[ Math.floor( Math.random() * 3 ) ]; // man, pin, so, char
   },
   calc: function( event ) {
     switch ( event.type ) {
@@ -13,8 +14,50 @@ var MahjongMan = {
     return;
   },
   _tsumo: function( draw ) {
+    var putPai;
     if ( draw.actor !== this.id ) {
       return;
+    }
+    switch ( draw.possible_actions ) {
+      case "hora":
+        return {
+          type: "hora",
+          actor: this.id,
+          target: this.id,
+          pai: draw.pai
+        };
+      default:
+        return this._getPutPai( draw );
+    }
+  },
+  _isNeededColor: function( pai ) {
+    if ( this._isWind( pai ) ) {
+      return true;
+    }
+    return ( pai.indexOf( this.color ) !== -1 );
+  },
+  _getUnneededPaiIndex: function() {
+    for ( var i in this.pais ) {
+      if ( !this._isNeededColor( this.pais[ i ] ) ) {
+        return i;
+      }
+    }
+  },
+  _isWind: function( pai ) {
+    return pai.indexOf( pai.toUpperCase() ) !== -1;
+  },
+  _getPutPai: function( draw ) {
+    // 狙っている色と同じ色の牌をツモったら、それ以外の色を捨てる。
+    if ( this._isNeededColor( draw.pai ) ) {
+      putPai = this.pais.splice( this._getUnneededPaiIndex(), 1, draw.pai );
+      if ( putPai ) {
+        return {
+          type: "dahai",
+          actor: this.id,
+          pai: putPai[ 0 ],
+          tsumogiri: false
+        };
+      }
     }
     return {
       type: "dahai",
